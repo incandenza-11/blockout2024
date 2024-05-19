ed1f3d8981cba91b34708fd6728f81916f3621b8
Sam <sam@MBP-de-Sam.technicolor.net>
2024-05-19 15:48:15 +0200

    Store already blocked users in cache to prevent attempting to block them again and speed up the execution
    - As at the time of writing this, Instagrapi does not provide access to the list of the current user's blocked accounts and Instagram also seems to keep track of scrapping activity within an account that could lead to ban, a custom solution is provided.
    - Add usernames to a local cache right after blocking them so that in the next execution those are skipped and so the execution is lighter.
    - Add usernames to the cache also when no user is found, as it is most likely that it has already been blocked before executing this script.
    - Closes issue #7

--- drivers/instagram.py
+++ drivers/instagram.py
@@ -14,35 +14,58 @@ class InstagramClient(BaseClient):
         super().__init__()
         self.usernames_filename = f'{PROJECT_ROOT_PATH}/resources/usernames/instagram.txt'
         self.cache_dir = f'{PROJECT_ROOT_PATH}/resources/cache/instagram'
+        self.blocked_users_filename = '{}/{}-blocked_users.txt'.format(self.cache_dir, '{username}')
         self.session_path_by_username = '{}/{}-session.json'.format(self.cache_dir, '{username}')
+        self.username: Optional[str] = None
         self.client = self.start_client()
 
-    def user_has_session(self, username: str) -> bool:
+    @staticmethod
+    def get_usernames_from_file(file_path: str) -> list[str]:
+        try:
+            with open(file_path, "r") as file:
+                print(f'Reading usernames from file: {file_path}')
+                return file.read().splitlines()
+        except FileNotFoundError:
+            raise Exception(f"Error: {file_path} file not found.")
+
+    @staticmethod
+    def _append_to_cache(filename: str, content: str) -> None:
+        with open(filename, 'a') as file:
+            file.write(f'{content}\n')
+
+    def _ensure_logged_in(self) -> None:
+        try:
+            self.client.get_timeline_feed()  # Checking if session is valid
+        except LoginRequired:
+            print('Session expired or invalid. Logging in again...')
+            self.client = self.start_client()
+
+    def _user_has_session(self, username: str) -> bool:
         return os.path.exists(self.session_path_by_username.format(username=username))
 
     def log_in(self, client: Client, username: str, password: str, verification_code: Optional[str] = None) -> None:
         client.login(username=username, password=password, verification_code=verification_code if verification_code else '')
-        if not self.user_has_session(username=username):
+        if not self._user_has_session(username=username):
             client.dump_settings(Path(self.session_path_by_username.format(username=username)))
 
     def start_client(self) -> Optional[Client]:
         print('Logging in to Instagram...')
-        username = input('Enter your Instagram username: ')
+        self.username = input('Enter your Instagram username: ')
         password = input('Enter your Instagram password: ')
         client = Client()
 
-        if self.user_has_session(username):
-            session_filename = self.session_path_by_username.format(username=username)
+        if self._user_has_session(self.username):
+            session_filename = self.session_path_by_username.format(username=self.username)
             client.load_settings(Path(session_filename))
 
         try:
-            self.log_in(client=client, username=username, password=password)
+            self.log_in(client=client, username=self.username, password=password)
 
         except TwoFactorRequired:
             print('Two-factor authentication required.')
             two_factor_code = input('Enter the 2FA code: ')
             try:
-                self.log_in(client=client, username=username, password=password, verification_code=two_factor_code)
+                self.log_in(client=client, username=self.username, password=password, verification_code=two_factor_code)
             except Exception as e2:
                 raise Exception(f'Two-factor login error: {e2}')
         except ChallengeRequired:
@@ -58,39 +81,39 @@ class InstagramClient(BaseClient):
         print('Successfully logged in to Instagram.')
         return client
 
-    def ensure_logged_in(self) -> None:
-        try:
-            self.client.get_timeline_feed()  # Checking if session is valid
-        except LoginRequired:
-            print('Session expired or invalid. Logging in again...')
-            self.client = self.start_client()
-
-    def get_usernames_from_file(self) -> list[str]:
-        try:
-            with open(self.usernames_filename, "r") as file:
-                print(f'Reading usernames from file: {self.usernames_filename}')
-                return file.read().splitlines()
-        except FileNotFoundError:
-            raise Exception(f"Error: {self.usernames_filename} file not found.")
-
     def get_user_id_from_username(self, username: str) -> Optional[str]:
         try:
-            self.ensure_logged_in()
+            self._ensure_logged_in()
             return self.client.user_info_by_username_v1(username).dict()['pk']
         except UserNotFound:
             print(f'User {username} has already been blocked or does not exist.')
             return None
 
     def block_users(self) -> None:
-        self.ensure_logged_in()
-        usernames_to_block = self.get_usernames_from_file()
+        self._ensure_logged_in()
+        usernames_to_block = self.get_usernames_from_file(file_path=self.usernames_filename)
+        blocked_users_file = self.blocked_users_filename.format(username=self.username)
+        blocked_usernames = []
+
+        if os.path.exists(blocked_users_file):
+            blocked_usernames.extend(self.get_usernames_from_file(file_path=blocked_users_file))
+        else:
+            # Create the cache file if it doesn't exist
+            with open(blocked_users_file, 'w'):
+                pass
 
         print("Blocking users...")
         for username in usernames_to_block:
+            if username in blocked_usernames:
+                print(f'User {username} is already blocked')
+                continue
+
             user_id = self.get_user_id_from_username(username)
             if user_id:
                 self.client.user_block(user_id)
                 print(f"User {username} with id {user_id} blocked successfully.")
+            self._append_to_cache(filename=blocked_users_file, content=username)
+
         print("Blocking complete.")
 
 
