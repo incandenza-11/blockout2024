8fb73b2bf9158a209c069a38665e17883f925785
Sam <sam@MBP-de-Sam.technicolor.net>
2024-05-19 14:39:00 +0200

    Enable sessions in Instagram login
    - Enable session in Instagram login as suggested in https://subzeroid.github.io/instagrapi/usage-guide/best-practices.html
    - Closes issue #10

--- drivers/instagram.py
+++ drivers/instagram.py
@@ -1,3 +1,5 @@
+import os.path
+from pathlib import Path
 from typing import Optional
 
 from instagrapi import Client
@@ -10,52 +12,58 @@ from drivers.base_client import BaseClient
 class InstagramClient(BaseClient):
     def __init__(self):
         super().__init__()
-        self.client = self.start_client()
-        if self.client is None:
-            raise Exception("Failed to initialize Instagram client.")
         self.usernames_filename = f'{PROJECT_ROOT_PATH}/resources/usernames/instagram.txt'
+        self.cache_dir = f'{PROJECT_ROOT_PATH}/resources/cache/instagram'
+        self.session_path_by_username = '{}/{}-session.json'.format(self.cache_dir, '{username}')
+        self.client = self.start_client()
 
-    @staticmethod
-    def start_client() -> Optional[Client]:
+    def user_has_session(self, username: str) -> bool:
+        return os.path.exists(self.session_path_by_username.format(username=username))
+
+    def log_in(self, client: Client, username: str, password: str, verification_code: Optional[str] = None) -> None:
+        client.login(username=username, password=password, verification_code=verification_code if verification_code else '')
+        if not self.user_has_session(username=username):
+            client.dump_settings(Path(self.session_path_by_username.format(username=username)))
+
+    def start_client(self) -> Optional[Client]:
         print('Logging in to Instagram...')
         username = input('Enter your Instagram username: ')
         password = input('Enter your Instagram password: ')
-
         client = Client()
 
+        if self.user_has_session(username):
+            session_filename = self.session_path_by_username.format(username=username)
+            client.load_settings(Path(session_filename))
+
         try:
-            client.login(username, password)
+            self.log_in(client=client, username=username, password=password)
+
         except TwoFactorRequired:
             print('Two-factor authentication required.')
             two_factor_code = input('Enter the 2FA code: ')
             try:
-                client.login(username, password, verification_code=two_factor_code)
+                self.log_in(client=client, username=username, password=password, verification_code=two_factor_code)
             except Exception as e2:
-                print(f'Two-factor login error: {e2}')
-                return None
+                raise Exception(f'Two-factor login error: {e2}')
         except ChallengeRequired:
             print('Challenge required. Please approve the login on your Instagram app or enter the code sent to your email/phone.')
             try:
                 client.challenge_resolve(client.last_json)
             except Exception as e2:
-                print(f'Challenge error: {e2}')
-                return None
+                raise Exception(f'Challenge error: {e2}')
         except Exception as e:
-            print(f'Login error: {e}')
-            return None
+            raise Exception(f'Error initializing Instagram client: {e}')
 
         client.delay_range = [1, 4]
         print('Successfully logged in to Instagram.')
         return client
 
-    def ensure_logged_in(self):
+    def ensure_logged_in(self) -> None:
         try:
             self.client.get_timeline_feed()  # Checking if session is valid
         except LoginRequired:
             print('Session expired or invalid. Logging in again...')
             self.client = self.start_client()
-            if self.client is None:
-                raise Exception("Failed to reinitialize Instagram client.")
 
     def get_usernames_from_file(self) -> list[str]:
         try:
@@ -75,7 +83,7 @@ class InstagramClient(BaseClient):
 
     def block_users(self) -> None:
         self.ensure_logged_in()
-        usernames_to_block = set(self.get_usernames_from_file())
+        usernames_to_block = self.get_usernames_from_file()
 
         print("Blocking users...")
         for username in usernames_to_block:
