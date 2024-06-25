1da70dc0c738d39016da923c7b9374575bd24aef
Sam <sam@MBP-de-Sam.technicolor.net>
2024-06-25 18:48:46 +0200

    Add Pylint to CI

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def mute_instagrapi_logger() -> None:
 
 
 def get_social_media_client() -> BaseClient:
-    available_options = ['1', '2']
-    selection = input(f'Select a social media (enter number):\n1) Instagram\n2) Twitter\n\n> ')
+    available_options = ["1", "2"]
+    selection = input("Select a social media (enter number):\n1) Instagram\n2) Twitter\n\n> ")
     if selection not in available_options:
         print(f"Invalid option. Available options are: {', '.join(available_options)}\n")
         get_social_media_client()
 
-    if selection == '1':
+    if selection == "1":
         return InstagramClient()
-    elif selection == '2':
-        return TwitterClient(cache_service=CacheService(filename=TWITTER_CACHE_PATH))
+
+    return TwitterClient(cache_service=CacheService(filename=TWITTER_CACHE_PATH))
 
 
 def main():
